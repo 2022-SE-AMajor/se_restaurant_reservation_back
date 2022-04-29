e0d2c723dfaed94a16c4bc0b180c5e8dbaacef17
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-04-30 08:24:54 +0900

    [구현] app - 초기세팅, 로그인

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express  from 'express';
+import cors from "cors";
+
+import { login } from './controller/auth';
+
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/login', login);
+
+app.listen(4000,()=>{
+    console.log('4000번 포트에서 대기중');
+  })
