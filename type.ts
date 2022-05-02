ab45e8ce240457e29c1a06d43a11d5aac5d41f04
Min dong Jo <65884076+sanso62@users.noreply.github.com>
2022-05-02 21:39:07 +0900

    Update type.ts
    ReservationProps type

--- type.ts
+++ type.ts
@@ -2,3 +2,12 @@ export type UserProps = {
     id:string,
     password:string
 }
+
+export type ReservationProps={
+    covers: number,
+    date: string,
+    time:string,
+    table_id:number,
+    customer_id:string,
+    arrival_time:any,
+}
