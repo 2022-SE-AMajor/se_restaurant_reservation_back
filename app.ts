90f7a2a317a46e8eeaff464ffba76d26a4db2f77
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-05-03 00:38:50 +0900

    DB연결

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express  from 'express';
 import cors from "cors";
 
 import { login } from './controller/auth';
+import { db } from './db/database';
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(express.json())
 app.use(cors());
 
 app.get('/login', login);
+
+
+db.getConnection().then((connection)=>console.log('Mysql 연결 완료'))
+
 
 app.listen(process.env.PORT||4000,()=>{
     console.log('4000번 포트에서 대기중');
