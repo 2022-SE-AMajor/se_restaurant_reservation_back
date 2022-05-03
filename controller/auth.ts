56a4eae48a95d875464ab12827386eb9b4a52341
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-05-03 10:08:26 +0900

    [구현] 로그인 기능 api

--- controller/auth.ts
+++ controller/auth.ts
@@ -1,13 +1,13 @@
 import * as userRepository from '../data/auth'
 
-import {UserProps}from '../type'
+import {UserProps, UserArrive}from '../type'
 
 import { Request, Response } from 'express';
 
 export async function login(req:Request,res:Response) {
     console.log('login',req.body)
     const {id, password} = req.body;
-    const found:UserProps = await userRepository.findUserById({id})
+    const found = await userRepository.findAdminById({id, password})
     if(!found) {
         return res.status(409).json({ message: `no id` });
     }
@@ -16,4 +16,4 @@ export async function login(req:Request,res:Response) {
     } else{
         return res.status(409).json({ message: `wrong password` });
     }
-}
+};
