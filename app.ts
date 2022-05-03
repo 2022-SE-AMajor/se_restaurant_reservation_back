90eb4221067150f307361a6f4a332866a698bc20
SmoothMonologue <mobugy99@gmail.com>
2022-05-03 13:52:43 +0900

    가자 천상계

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express  from 'express';
 import cors from "cors";
 
 import { login } from './controller/auth';
+import { db } from './db/database';
 
 const app = express();
 
@@ -10,6 +11,45 @@ app.use(cors());
 
 app.get('/login', login);
 
+db.getConnection().then((connection:any)=>{
+  console.log("MYSQL 연결완료");
+})
+
 app.listen(process.env.PORT||4000,()=>{
     console.log('4000번 포트에서 대기중');
-  })
+}); 
+app.get('/', (요청, 응답) => {
+  응답.send('갔냐?');
+});
+app.post('/', (요청, 응답) => {
+  응답.send('ok?');
+});
+app.post('/api/book/date', function(요청, 응답){
+  const {date} =요청.body;
+  console.log(date);
+  if (date == "0502") {
+    let data=[{
+      이름: "김호진",
+      시각: "15:00"
+    },
+    {
+      이름: "김손님",
+      시각: "16:00"
+    }];
+    응답.send(data);
+  } else 응답.send("예약할 수 없는 날짜입니다.");
+});
+app.post('/api/list/table', function(요청, 응답){
+
+  응답.send({
+    result: db.query('select * from 식탁'),
+    isSuccess: false,
+    code:200,
+    message: "데이터가 누락됐습니다."
+  });
+
+});
+app.post('/api/modify/:date', function(요청, 응답){
+  let {date} = 요청.params;
+
+});
--- db/database.ts
+++ db/database.ts
@@ -3,8 +3,8 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
     host:'localhost',
     user: 'root',
-    database: 'restaurantreservation',
-    password: '1111',
+    database: '식당예약',
+    password: 'NegaTive!TacKle99',
 })
 
 export const db = pool.promise()
