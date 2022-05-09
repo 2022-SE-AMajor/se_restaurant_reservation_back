292956df14a9b970496027b5781e715ff597a40f
sehyun <goozip2@gmail.com>
2022-05-09 22:41:01 +0900

    deleteData, deleteController 1차 업로드

---
+++ controller/deleteController.ts
@@ -0,0 +1,59 @@
+
+const {deleteReservation}=require('../data/deleteData');
+// import { ReservationProps } from "../type";
+import { Request, Response } from 'express';
+
+export async function createReservation(req: Request, res:Response){
+    const { covers, date, time, table_id, customer_id, arrival_time}=req.body;
+    console.log(covers, date, time, table_id, customer_id, arrival_time);
+
+    const deleteReservationRow = await deleteReservation(
+        covers,
+        date,
+        time,
+        table_id,
+        customer_id,
+        arrival_time
+    );
+    
+    if(deleteReservationRow) {
+        return res.send({
+            isSuccess: true,
+            code: 200,
+            message: "예약 삭제 성공"
+        })
+    } else {
+        return res.send({
+            isSuccess: false,
+            code: 400,
+            message: "예약 삭제 실패"
+        })
+    }
+
+};
+
+
+// const insertData= require("../data/insertData.js");
+
+// exports.createReservation =async function(req, res){
+//     const { covers, date, time, table_id, customer_id, arrival_time}=req.body;
+//     console.log(covers, date, time, table_id, customer_id, arrival_time);
+    
+//     const insertReservationRow = await insertData.insertReservation(
+//         covers,
+//         date,
+//         time,
+//         table_id,
+//         customer_id,
+//         arrival_time
+//     );
+    
+//     if(!insertReservationRow) {
+//         return res.send({
+//             isSuccess: false,
+//             code: 400,
+//             message: "예약 실패"
+//         })
+//     }
+
+// };
