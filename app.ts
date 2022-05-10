ea8e58016e3a790c89e9e5c18ebb85769d4d90f3
sanso62 <jmd9762@gmail.com>
2022-05-10 21:25:49 +0900

    예약 년월일시간으로 조회 그리고 현재시간 전 예약은 조회 불가

--- app.ts
+++ app.ts
@@ -10,10 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 app.post("/login", login);
 app.post("/reserve", createReservation);
-app.get("/readReservation", readReservation);
+app.post("/readReservation", readReservation);
 
 app.listen(process.env.PORT || 4000, () => {
     console.log("4000번 포트에서 대기중");
--- data/readData.ts
+++ data/readData.ts
@@ -1,6 +1,5 @@
 const { readPool } = require("../db/database.ts");
 
-
 // insertController에서 중복되는지 확인하는 용도
 exports.selectDateTimeAndTableId = async function () {
     const connection = await readPool.getConnection(async (conn: any) => conn);
@@ -14,7 +13,27 @@ exports.selectDateTimeAndTableId = async function () {
         connection.release();
         return row;
     } catch (err) {
-        console.error("readReservation query error");
+        console.error("query error");
+        connection.release();
+        return false;
+    }
+};
+
+// readController에서 날짜 시간으로 조회
+exports.selectReservation = async function (selectedDate: any, selectedTime: any) {
+    const connection = await readPool.getConnection(async (conn: any) => conn);
+    console.log(selectedDate + selectedTime);
+    console.log("connection done");
+    try {
+        const query = "select table_id, name, phone_number from reservation where date=? and time=?;";
+        const params = [selectedDate, selectedTime];
+        const [row] = await connection.query(query, params);
+        console.log(row);
+        console.log("query done");
+        connection.release();
+        return row;
+    } catch (err) {
+        console.error("query error");
         connection.release();
         return false;
     }
