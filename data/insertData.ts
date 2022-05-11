7c902cb1d7a9140f73241676809915d5cb030657
SmoothMonologue <mobugy99@gmail.com>
2022-05-11 16:11:57 +0900

    당장 테스트할 수 있게 다듬기

--- data/insertData.ts
+++ data/insertData.ts
@@ -1,32 +1,37 @@
-
-const {insertPool}=require("../db/database.ts");
+const { insertPool } = require("../db/database.ts");
 // import {pool} from "../db/database";
 // console.log(pool);
 
-exports.insertReservation = async function(covers:any, date:any, time:any, table_id:any, customer_id:any, arrival_time:any){
-    const connection = await insertPool.getConnection(async (conn:any) => conn);
+exports.insertReservation = async function (
+    covers: any,
+    date: any,
+    time: any,
+    table_id: any,
+    customer_id: any,
+    arrival_time: any,
+) {
+    const connection = await insertPool.getConnection(async (conn: any) => conn);
     // console.log(connection);
-    console.log("connection done")
-    try{
-        const query="insert into reservation(covers, date, time, table_id, customer_id, arrival_time) values(?,?,?,?,?,?);";
-        const params=[covers, date, time, table_id, customer_id, arrival_time];
-        const [row]=await connection.query(query, params);
+    console.log("connection done");
+    try {
+        const query =
+            "insert into reservation(covers, date, time, table_id, customer_id, arrival_time) values(?,?,?,?,?,?);";
+        const params = [covers, date, time, table_id, customer_id, arrival_time];
+        const [row] = await connection.query(query, params);
         // console.log(row);
-        console.log("query done")
+        console.log("query done");
         connection.release();
         return row;
-    }catch(err){
+    } catch (err) {
         console.error("insertReservation query error");
         connection.release();
         return false;
     }
-    
-}
+};
 
 //test
 // const {pool}=require("../db/database.ts");
 // // import {pool} from "../db/database";
-
 
 // exports.insertReservation=async function(covers, date, time, table_id, customer_id, arrival_time){
 //     const connection = await pool.getConnection(async (conn) => conn);
@@ -43,4 +48,3 @@ exports.insertReservation = async function(covers:any, date:any, time:any, table
 //     //     connection.release();
 //     //     return false;
 //     // }
-    
--- data/statOfData.ts
+++ data/statOfData.ts
@@ -1,22 +1,24 @@
-const {statPool}=require("../db/database.ts");
+const { statPool } = require("../db/database.ts");
 //선택한 달의 통계 읽기
 
-exports.showStats = async function (chosenYM:any) {
+exports.selectStats = async function (/*chosenYM: any*/) {
     const connection = await statPool.getConnection(async (conn: any) => conn);
     console.log(`connection done.`);
-    try{
-        const query="select * from stat where Year&Month=? OR Year&Month=?";
+    try {
+        //const query = "select * from stat where year_month=? OR year_month=?";
+        const query = "select * from stat";
         //저번 달 대비 노쇼비율을 위해 저번달 통계도 같이 선택
+        //일단 다 골라
         let params;
-        if(chosenYM%20==1) params=[chosenYM, chosenYM-89]; //1월이면 작년 12월 선택
-        else params=[chosenYM, chosenYM-1];
-        const [row]=await connection.query(query, params);
+        /*if (chosenYM % 20 == 1) params = [chosenYM, chosenYM - 89]; //1월이면 작년 12월 선택
+        else params = [chosenYM, chosenYM - 1];*/
+        const [row] = await connection.query(query);
         console.log(`query done.`);
         connection.release();
         return row;
-    }catch(err){
+    } catch (err) {
         console.error("showStats query error");
         connection.release();
         return false;
     }
-}
+};
