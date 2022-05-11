82ec4863793b8d6fbeb1dda3b572b18209898f20
SmoothMonologue <mobugy99@gmail.com>
2022-05-12 03:49:32 +0900

    4명부터 12명까지는 3명 이상으로 받도록 설정

--- data/statOfData.ts
+++ data/statOfData.ts
@@ -23,7 +23,7 @@ exports.selectStats = async function (chosenYM: any) {
     console.log(`connection done.`);
     try {
         const query = "select * from stat where `year_month`=? OR `year_month`=?";
-        //저번 달 대비 노쇼비율을 위해 저번달 통계도 같이 선택
+        //저번 달 대비 노쇼비율을 위해 저번달 통계도 같이 선택, 저번 달 통계가 존재하지 않으면 걍 선택한 달만 보여줌
         //쿼리문에 year_month 양 옆에 ` 안 넣으면 인식 못 함 주의, 이유는 불명, sql에 year과 month가 이미 있는 변수라 못 쓰나?
         let params;
         if (chosenYM % 20 == 1) params = [chosenYM, chosenYM - 89]; //1월이면 작년 12월 선택
--- data/updateStat.ts
+++ data/updateStat.ts
@@ -6,34 +6,77 @@ exports.updateStat = async function (thisYM: any, noShow: boolean, day: any, peo
     try {
         switch (day) {
             case `월`:
-                const query1 = "update stat set Mon=Mon+1 where `year_month`=?";
-                await connection.query(query1, thisYM);
+                const dayQuery1 = "update stat set Mon=Mon+1 where `year_month`=?";
+                await connection.query(dayQuery1, thisYM);
+                break;
+            case `화`:
+                const dayQuery2 = "update stat set Tue=Tue+1 where `year_month`=?";
+                await connection.query(dayQuery2, thisYM);
+                break;
+            case `수`:
+                const dayQuery3 = "update stat set Wed=Wed+1 where `year_month`=?";
+                await connection.query(dayQuery3, thisYM);
+                break;
+            case `목`:
+                const dayQuery4 = "update stat set Thu=Thu+1 where `year_month`=?";
+                await connection.query(dayQuery4, thisYM);
+                break;
+            case `금`:
+                const dayQuery5 = "update stat set Fri=Fri+1 where `year_month`=?";
+                await connection.query(dayQuery5, thisYM);
+                break;
+            case `토`:
+                const dayQuery6 = "update stat set Sat=Sat+1 where `year_month`=?";
+                await connection.query(dayQuery6, thisYM);
+                break;
+            case `일`:
+                const dayQuery7 = "update stat set Sun=Sun+1 where `year_month`=?";
+                await connection.query(dayQuery7, thisYM);
                 break;
             default:
-                //유효하지 않은 요일
+                console.log(`유효하지 않은 요일입니다.`);
                 break;
         }
         switch (people) {
             //스위치 문을 숫자로 비교할 순 없나?
             case 1:
-                const query2 = "upsate stat set oneC=oneC+1 where `year_month`=?";
-                await connection.query(query2, thisYM);
+                const numQuery1 = "update stat set oneC=oneC+1 where `year_month`=?";
+                await connection.query(numQuery1, thisYM);
+                break;
+            case 2:
+                const numQuery2 = "update stat set twoC=twoC+1 where `year_month`=?";
+                await connection.query(numQuery2, thisYM);
+                break;
+            case 3:
+                const numQuery3 = "update stat set threeC=threeC+1 where `year_month`=?";
+                await connection.query(numQuery3, thisYM);
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                const numQuery4 = "update stat set more_than_threeC=more_than_threeC+1 where `year_month`=?";
+                await connection.query(numQuery4, thisYM);
                 break;
             default:
-                //지원하지 못하는 규모거나 유효하지 않은 인원
+                console.log(`인원이 테이블 규모보다 많거나 유효하지 않은 입력입니다.`);
                 break;
         }
         //언제 노쇼라고 판단하고 반영할지는 미정
         //하루가 지나면 노쇼가 몇 번 있었는지 파악한 후 그만큼 추가하는 방법도 있음.
         if (noShow == true) {
-            const query3 = "update stat set no_show=no_show+1 where `year_month`=?";
-            await connection.query(query3, thisYM);
+            const absQuery = "update stat set no_show=no_show+1 where `year_month`=?";
+            await connection.query(absQuery, thisYM);
         }
 
         const query = "update stat set month_total=month_total+1 where `year_month`=?";
         //month_total+=1이나 month_total++은 못하지만 저건 된다.
         await connection.query(query, thisYM);
-
         console.log(`query done.`);
         connection.release();
         return "잘 수정됐다구~";
