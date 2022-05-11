1fcf863d0ebd426ba1542567ff7b3580ca40ff7d
SmoothMonologue <mobugy99@gmail.com>
2022-05-11 16:13:42 +0900

    pool 추가

--- db/database.ts
+++ db/database.ts
@@ -1,10 +1,14 @@
-import mysql from 'mysql2';
+import mysql from "mysql2";
 
 const pool = mysql.createPool({
-    host:'localhost',
-    user: 'root',
-    database: 'restaurantreservation',
-    password: '1111',
-})
+    host: "localhost",
+    user: "root",
+    database: "restaurantreservation",
+    password: "1111",
+});
 
-export const db = pool.promise()
+export const db = pool.promise();
+export const readPool = pool.promise();
+export const insertPool = pool.promise();
+export const statPool = pool.promise();
+export const createStatPool = pool.promise();
