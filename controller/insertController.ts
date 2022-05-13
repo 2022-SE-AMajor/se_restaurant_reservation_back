96afd89da036329fe897a91fa749cf9085f27997
MyeongSeok98 <myong2404@gmail.com>
2022-05-14 06:04:47 +0900

    삭제와 삽입

--- controller/insertController.ts
+++ controller/insertController.ts
@@ -1,43 +1,34 @@
-
-const {insertReservation}=require('../data/insertData');
+const { insertReservation } = require("../data/insertData");
 // import { ReservationProps } from "../type";
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-export async function createReservation(req: Request, res:Response){
-    const { covers, date, time, table_id, customer_id, arrival_time}=req.body;
+export async function createReservation(req: Request, res: Response) {
+    const { covers, date, time, table_id, customer_id, arrival_time } = req.body;
     console.log(covers, date, time, table_id, customer_id, arrival_time);
 
-    const insertReservationRow = await insertReservation(
-        covers,
-        date,
-        time,
-        table_id,
-        customer_id,
-        arrival_time
-    );
-    
-    if(insertReservationRow) {
+    const insertReservationRow = await insertReservation(covers, date, time, table_id, customer_id, arrival_time);
+
+    if (insertReservationRow) {
         return res.send({
             isSuccess: true,
             code: 200,
-            message: "예약 성공"
-        })
+            message: "예약 성공",
+        });
     } else {
         return res.send({
             isSuccess: false,
             code: 400,
-            message: "예약 실패"
-        })
+            message: "예약 실패",
+        });
     }
-
-};
+}
 
 // const insertData= require("../data/insertData.js");
 
 // exports.createReservation =async function(req, res){
 //     const { covers, date, time, table_id, customer_id, arrival_time}=req.body;
 //     console.log(covers, date, time, table_id, customer_id, arrival_time);
-    
+
 //     const insertReservationRow = await insertData.insertReservation(
 //         covers,
 //         date,
@@ -46,7 +37,7 @@ export async function createReservation(req: Request, res:Response){
 //         customer_id,
 //         arrival_time
 //     );
-    
+
 //     if(!insertReservationRow) {
 //         return res.send({
 //             isSuccess: false,
