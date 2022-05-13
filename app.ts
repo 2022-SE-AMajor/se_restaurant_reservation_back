72a2128c1679a96c8c34f65b51f0ec27704c131e
MyeongSeok98 <myong2404@gmail.com>
2022-05-14 05:50:10 +0900

    도착기록 구현완료

--- app.ts
+++ app.ts
@@ -1,20 +1,21 @@
-import express  from 'express';
+import express from "express";
 import cors from "cors";
 
-import { login } from './controller/auth';
-import {createReservation} from './controller/insertController';
-import {readReservation} from './controller/readController';
+import { login } from "./controller/auth";
+import { createReservation } from "./controller/insertController";
+import { readReservation } from "./controller/readController";
+import { arriveTime } from "./controller/arriveController";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-
-app.post('/login', login);
+app.get("/login", login);
 app.post("/reserve", createReservation);
 app.get("/readReservation", readReservation);
+app.post("/arrivetime", arriveTime);
 
-app.listen(process.env.PORT||4000,()=>{
-    console.log('4000번 포트에서 대기중');
-  })//test
+app.listen(process.env.PORT || 4000, () => {
+    console.log("4000번 포트에서 대기중");
+}); //test
---
+++ controller/arriveController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+
+const arriveData = require("../data/arriveData");
+
+export async function arriveTime(req: Request, res: Response) {
+    console.log("table_id", req.body);
+    const { table_id } = req.body;
+    console.log(table_id);
+    //const found: UserArrive = await findArriveTime({ oid });
+    const found = await arriveData.findArriveTime(table_id);
+    console.log(found[0][`table_id`]);
+    if (!found) {
+        return res.status(409).json({ message: `error` });
+    }
+    if (found[0][`table_id`] == table_id) {
+        const oid = found[0][`oid`];
+        console.log(oid);
+        const arriveFound = await arriveData.insertArrival(table_id);
+
+        if (arriveFound) {
+            return res.send({
+                isSuccess: true,
+                code: 200,
+                message: "도착시간 기록 성공",
+            });
+        } else {
+            return res.send({
+                isSuccess: false,
+                code: 400,
+                message: "도착시간 기록 실패",
+            });
+        }
+    } else {
+        return res.status(409).json({ message: `해당 테이블은 예약이 없습니다.` });
+    }
+}
