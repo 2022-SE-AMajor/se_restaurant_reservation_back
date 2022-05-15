730e5ad7c8740e6f504dafa7da81455c818796a7
sanso62 <jmd9762@gmail.com>
2022-05-15 21:15:07 +0900

    complete reading reservation info

--- controller/readController.ts
+++ controller/readController.ts
@@ -1,33 +1,43 @@
-const { selectReservation } = require("../data/readData");
+const { selectTableIdList, selectReservation } = require("../data/readData");
 import { Request, Response } from "express";
 
-export async function readReservation(req: Request, res: Response) {
+export async function isValidDateTimeWhenReading(req: Request, res: Response) {
     const { year, month, date, time } = req.body;
     console.log(year, month, date, time);
-    const selectedDate = year + "-" + month + "-" + date;
-    const selectedTime = time;
     let now = new Date(); // 한국시간 기준 아님
-    let dateTime = new Date(year + "-" + month + "-" + date + "T" + time); //한국시간 기준 아님
+    let dateTime = new Date(`${year}-${month}-${date}T${time}`); //한국시간 기준 아님
     console.log(now);
     console.log(dateTime);
     if (now > dateTime) {
-        console.log("에러: 지난 날짜");
-        const html = ` 
-            <!DOCTYPE html>
-            <html>
-            <head>
-            <meta charset="UTF-8">
-            <title>practice</title>
-            </head>
-            <body>
-            <h1>에러: 지난날짜</h1>
-            </body>
-            </html>
-            `; //front 메세지 창 html
-        return res.send(html);
+        console.log("에러: 지난 날짜입니다.");
+        return res.send({
+            isSuccess: false,
+            code: 400,
+            message: "에러: 지난 날짜입니다.",
+        });
     }
+    const selectTableIdListRow = await selectTableIdList(`${year}-${month}-${date}`, time);
 
-    const selectRervationRow = await selectReservation(selectedDate, selectedTime);
+    if (selectTableIdListRow) {
+        return res.send({
+            result: selectTableIdListRow,
+            isSuccess: true,
+            code: 200,
+            message: "선택한 시간에 테이블리스트를 조회 했습니다.",
+        });
+    } else {
+        return res.send({
+            isSuccess: false,
+            code: 400,
+            message: "에러: 선택한 시간에 테이블리스트를 조회 할 수 없습니다.",
+        });
+    }
+}
+
+export async function readReservation(req: Request, res: Response) {
+    const { date, time } = req.query;
+    console.log(date, time);
+    const selectRervationRow = await selectReservation(date, time);
     // console.log(selectRervationRow);
 
     if (selectRervationRow) {
@@ -35,13 +45,13 @@ export async function readReservation(req: Request, res: Response) {
             result: selectRervationRow,
             isSuccess: true,
             code: 200,
-            message: "예약 조회 성공",
+            message: "선택한 시간에 예약정보를 조회했습니다.",
         });
     } else {
         return res.send({
             isSuccess: false,
             code: 400,
-            message: "예약 조회 실패",
+            message: "에러: 선택한 시간에 예약정보를 조회할 수 없습니다.",
         });
     }
 }
--- data/readData.ts
+++ data/readData.ts
@@ -20,36 +20,36 @@ exports.selectTableIdList = async function (date: any, time: any) {
 };
 
 // insertController에서 중복되는지 확인하는 용도
-exports.selectDateTimeAndTableId = async function () {
-    const connection = await readPool.getConnection(async (conn: any) => conn);
-    // console.log(connection);
-    console.log("connection done");
-    try {
-        const query = "select date, time, table_id from reservation";
-        const [row] = await connection.query(query);
-        // console.log(row);
-        console.log("query done");
-        connection.release();
-        return row;
-    } catch (err) {
-        console.error("query error");
-        connection.release();
-        return false;
-    }
-};
+// exports.selectDateTimeAndTableId = async function () {
+//     const connection = await readPool.getConnection(async (conn: any) => conn);
+//     // console.log(connection);
+//     console.log("connection done");
+//     try {
+//         const query = "select date, time, table_id from reservation";
+//         const [row] = await connection.query(query);
+//         // console.log(row);
+//         console.log("query done");
+//         connection.release();
+//         return row;
+//     } catch (err) {
+//         console.error("query error");
+//         connection.release();
+//         return false;
+//     }
+// };
 
 // readController에서 날짜 시간으로 조회
-exports.selectReservation = async function (selectedDate: any, selectedTime: any) {
+exports.selectReservation = async function (date: any, time: any) {
+    console.log(date, time);
     const connection = await readPool.getConnection(async (conn: any) => conn);
-    console.log(selectedDate + selectedTime);
     console.log("connection done");
     try {
-        const query = "select table_id, name, phone_number from reservation where date=? and time=?;";
-        const params = [selectedDate, selectedTime];
+        const query = "select covers, table_id, name, phone_number from reservation where date=? and time=?;";
+        const params = [date, time];
         const [row] = await connection.query(query, params);
         console.log(row);
-        console.log("query done");
         connection.release();
+        console.log("query done");
         return row;
     } catch (err) {
         console.error("query error");
