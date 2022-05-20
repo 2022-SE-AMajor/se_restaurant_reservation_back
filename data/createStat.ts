8e27f65390cf2a0801076cb5f8235545e5e0d194
SmoothMonologue <mobugy99@gmail.com>
2022-05-20 13:01:35 +0900

    필요없어진 더미 통계 함수 삭제

--- data/createStat.ts
+++ data/createStat.ts
@@ -1,5 +1,3 @@
-//새로운 통계 데이터 생성은 언제?
-//이러면 시간이 지날수록 데이터가 많이 쌓이는데 1년 이상 지났다거나 너무 오래된 건 보존할 필요가 있을까?
 const { createStatPool } = require("../db/database.ts");
 
 exports.createStat = async function () {
